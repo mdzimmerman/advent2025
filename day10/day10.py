59fe1226bfb776078a24e81d0e634756d8967b9f
Matthew Zimmerman <matt.zimmerman@ccri.com>
2026-01-20 09:48:15 -0500

    more work on day 10

--- day10/day10.py
+++ day10/day10.py
@@ -1,4 +1,33 @@
+from collections import deque, namedtuple
+
 import aoc
+import numpy as np
+import sympy
+
+def itersum(dim, nmax):
+    if dim == 1:
+        return itersum1(nmax)
+    elif dim == 2:
+        return itersum2(nmax)
+    elif dim == 3:
+        return itersum3(nmax)
+    else:
+        raise RuntimeError("can't iterate over 3 dimensions")
+
+def itersum1(nmax):
+    for x1 in range(nmax+1):
+        yield (x1,)
+
+def itersum2(nmax):
+    for x1 in range(nmax+1):
+        for x2 in range(nmax-x1+1):
+            yield (x1, x2)
+
+def itersum3(nmax):
+    for x1 in range(nmax+1):
+        for x2 in range(nmax-x1+1):
+            for x3 in range(nmax-x1-x2+1):
+                yield (x1, x2, x3)
 
 class Machine:
 
@@ -13,7 +42,7 @@ class Machine:
 
         self.sbuttons = list()
         for sb in ss[1:-1]:
-            self.sbuttons.append([int(n) for n in sb[1:-1].split(",")])
+            self.sbuttons.append(set(int(n) for n in sb[1:-1].split(",")))
         self.buttons = list()
         for sb in self.sbuttons:
             self.buttons.append(self.parse_list(sb))
@@ -40,17 +69,104 @@ class Machine:
                 out += "."
         return out
 
+    def find_part1(self):
+        State = namedtuple("State", ["lights", "n", "pushed", "left"])
+
+        left = set(range(len(self.buttons)))
+        start = State(lights=0, n=0, pushed=set(), left=left)
+        queue = deque()
+        queue.append(start)
+
+        while queue:
+            state = queue.popleft()
+            if state.lights == self.init:
+                #print(state)
+                return state.n
+            for i in state.left:
+                queue.append(State(state.lights ^ self.buttons[i], state.n+1, state.pushed|{i}, state.left-{i}))
+        return None
+
+    def rref(self, mat):
+        rref_sympy, pivot_cols = sympy.Matrix(mat).rref()
+        return np.array(rref_sympy, dtype=np.int32), pivot_cols
+
+    def part2(self):
+        #print(self.sbuttons)
+        #print(self.buttons)
+        nb = len(self.sbuttons)
+        #print(self.length, nb)
+        A = np.zeros((self.length, nb), dtype=np.int32)
+        for i, sb in enumerate(self.sbuttons):
+            for j in sb:
+                A[j, i] = 1
+        y = np.array(self.joltage, dtype=np.int32)[:, np.newaxis]
+        #print(A)
+        #print(y)
+        Ay = np.concatenate((A, y), axis=1)
+        print(Ay)
+        Ay_rref, depvar = self.rref(Ay)
+        print(Ay_rref)
+        inpvar = list()
+        for i in range(nb):
+            if i not in depvar:
+                inpvar.append(i)
+
+        #print(inpvar, depvar)
+        #x = np.array((1, 3, 0, 3, 1, 2), dtype=np.int32)
+        #print(A @ x.T)
+
+        nmax = sum(Ay_rref[:, -1])
+        bmin = nmax
+        if not inpvar:
+            print(bmin)
+            return bmin
+
+        for xis in itersum(len(inpvar), nmax):
+            x = np.zeros((nb), dtype=np.int32)
+            z = Ay_rref[:, -1]
+            for i, xi in zip(inpvar, xis):
+                z = z - xi * Ay_rref[:, i]
+                x[i] = xi
+            for i, dv in enumerate(depvar):
+                x[dv] = z[i]
+            if np.all(x >= 0) and sum(x) <= bmin:
+                bmin = sum(x)
+                print(bmin, x, "=>", A @ x.T)
+        return bmin
+
+
     def __repr__(self):
-        return f'{type(self).__name__}(init={self.init} buttons={self.buttons} joltage={self.joltage})'
+        return f'{type(self).__name__}(init={self.init} sbuttons={self.sbuttons} buttons={self.buttons} joltage={self.joltage})'
 
     @classmethod
     def from_file(cls, filename):
         return [cls(s) for s in aoc.read_lines(filename)]
 
+def part1(machines, debug=False):
+    count = 0
+    for m in machines:
+        mc = m.find_part1()
+        if debug: print(mc, m)
+        count += mc
+    return count
+
+def part2(machines, debug=False):
+    count = 0
+    for m in machines:
+        mc = m.part2()
+        if debug: print(mc, m)
+        count += mc
+    return count
+
 if __name__ == '__main__':
     test = Machine.from_file("test.txt")
-    test0 = test[0]
-    print(test0.draw(0))
-    print(test0.draw((0 ^ test0.buttons[4]) ^ test0.buttons[5]))
-    print(test0.draw((0 ^ test0.buttons[5]) ^ test0.buttons[4]))
-    print(test0.draw(test0.init))
+    #print(test[0])
+    #print(part1(test))
+    #test[0].part2()
+    print(part2(test, debug=True))
+
+    #for x in itersum(1, 10):
+    #    print(x)
+
+    #inp = Machine.from_file("input.txt")
+    #print(part1(inp))
